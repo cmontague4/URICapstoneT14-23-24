8df0623f12a5a7fce99880c9c083b82d9256c1e5
Connor Montague <90429535+cmontague4@users.noreply.github.com>
2024-02-22 14:37:45 -0500

    Update rad_runner.py

--- python/rad_runner.py
+++ python/rad_runner.py
@@ -4,5 +4,10 @@
 
 
 import serial
+import time
 
-print('hello world')
+reader = serial.Serial(port = '/dev/ttyUSB0', baudrate = 9600, timeout = 0.05)
+
+while True:
+    data = reader.readline().decode().strip()
+    print(data)
